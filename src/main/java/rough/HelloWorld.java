40ed4b9026e1ec378c9cc2c8d40ace1f82df8682
Yogesh <qayogesh7@gmail.com>
2018-07-01 15:12:57 +0530

    This is yet another commit

--- src/main/java/rough/HelloWorld.java
+++ src/main/java/rough/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("This is ust a demo");
+		System.out.println("This is just to demostrate the EGit functionlity");
 		
 	}
 
